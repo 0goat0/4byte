c0c2d968b8e5b3a2b8251397bd1ad658ff783830
chan527 <chan020527@gmail.com>
2026-09-05 03:11:05 +0900

    feat: 네트워크 NavMesh 이동 및 순찰 테스트 구현
    - State Authority에서 NavMesh 이동 처리
    - NetworkTransform을 통한 위치 동기화
    - 이동, 정지 및 목적지 도착 이벤트 구현
    - 순찰 상태머신 사용 예제 추가

---
+++ Assets/_Project/01_Scripts/Movement/NavMeshTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Fusion;
+using UnityEngine;
+
+public class NavMeshTest : NetworkBehaviour
+{
+    [SerializeField] private List<Transform> _patrolPoints = new();
+
+    private NetworkNavMeshMover _mover;
+    private int _currentPointIndex;
+
+    private void Awake()
+    {
+        _mover = GetComponent<NetworkNavMeshMover>();
+    }
+
+    public override void Spawned()
+    {
+        if (!Object.HasStateAuthority || _patrolPoints.Count == 0)
+            return;
+
+        _mover.OnDestinationReached += MoveToNextPoint;
+        MoveToCurrentPoint();
+    }
+
+    public override void Despawned(NetworkRunner runner, bool hasState)
+    {
+        _mover.OnDestinationReached -= MoveToNextPoint;
+    }
+
+    private void MoveToCurrentPoint()
+    {
+        Transform currentPoint = _patrolPoints[_currentPointIndex];
+
+        if (currentPoint == null)
+            return;
+
+        _mover.MoveTo(currentPoint.position);
+    }
+
+    private void MoveToNextPoint()
+    {
+        _currentPointIndex =
+            (_currentPointIndex + 1) % _patrolPoints.Count;
+
+        MoveToCurrentPoint();
+    }
+}
---
+++ Assets/_Project/01_Scripts/Movement/NetworkNavMeshMover.cs
@@ -0,0 +1,69 @@
+﻿using Fusion;
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+// 컴포넌트에 NavMeshAgent 가 없으면 자동으로 추가
+[RequireComponent(typeof(NavMeshAgent), typeof(NetworkTransform))]
+public class NetworkNavMeshMover : NetworkBehaviour
+{
+    // 목적지
+    [SerializeField] private Vector3 _destination;
+
+    private const float ArrivalSpeedSqr = 0.01f;
+
+    private NavMeshAgent _agent;
+    private bool _hasActiveDestination;
+
+    public event Action OnDestinationReached;
+
+    private void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    public override void Spawned()
+    {
+        _agent.enabled = Object.HasStateAuthority;
+    }
+
+    public override void FixedUpdateNetwork()
+    {
+        if (!HasReachedDestination())
+            return;
+        
+        _hasActiveDestination = false;
+        OnDestinationReached?.Invoke();
+    }
+
+    private bool HasReachedDestination()
+    {
+        if (!_hasActiveDestination || _agent.pathPending)
+            return false;
+
+        bool isWithinStoppingDistance = _agent.remainingDistance <= _agent.stoppingDistance;
+
+        bool isMovingSlowEnough = _agent.velocity.sqrMagnitude <= ArrivalSpeedSqr;
+
+        return isWithinStoppingDistance && isMovingSlowEnough;
+    }
+
+    public void MoveTo(Vector3 destination)
+    {
+        if (!Object.HasStateAuthority)
+            return;
+
+        _destination = destination;
+        _hasActiveDestination = _agent.SetDestination(_destination);
+    }
+
+    public void Stop()
+    {
+        if (!Object.HasStateAuthority)
+            return;
+
+        _agent.ResetPath();
+
+        _hasActiveDestination = false;
+    }
+}

056cd0cd667bc70522849d1741d71cda8f52fbd6
0goat0 <0tiftm00@gmail.com>
2026-09-23 18:31:41 +0900

    Minimap 카메라 세팅

---
+++ Assets/_Project/01_Scripts/UI/MiniMapManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MiniMapManager : MonoBehaviour, IPointerClickHandler, IDragHandler
+{
+    public Camera minimapCamera;
+    public Transform mainCameraTarget;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        MoveMainCamera(eventData);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        MoveMainCamera(eventData);
+    }
+
+    private void MoveMainCamera(PointerEventData eventData)
+    {
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(transform as RectTransform, eventData.position, eventData.pressEventCamera, out Vector2 localPoint);
+        Rect rect = (transform as RectTransform).rect;
+
+        float xPercent = Mathf.InverseLerp(rect.xMin, rect.xMax, localPoint.x);
+        float yPercent = Mathf.InverseLerp(rect.yMin, rect.yMax, localPoint.y);
+
+        Ray ray = minimapCamera.ViewportPointToRay(new Vector3(xPercent, yPercent, 0));
+
+        if(Physics.Raycast(ray, out RaycastHit hit))
+        {
+            Vector3 targetPosition =hit.point;
+            targetPosition.y = mainCameraTarget.position.y;
+            mainCameraTarget.position = targetPosition;
+        }
+    }
+}
---
+++ Assets/_Project/01_Scripts/UI/MinimapView.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class MinimapView : MonoBehaviour
+{
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private float minimapLineHeight = 1.0f; //높이(Y축)
+
+    private LineRenderer lineRenderer;
+    private Vector3[] viewportPoints = new Vector3[3];
+    private Vector3[] worldPoints = new Vector3[3];
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+
+        //모서리 Viewport 좌표 설정
+        viewportPoints[0] = new Vector3(0, 0, 0);
+        viewportPoints[1] = new Vector3(1, 0, 0);
+        viewportPoints[2] = new Vector3(1, 1, 0);
+        viewportPoints[3] = new Vector3(0, 1, 0);
+    }
+
+    private void LateUpdate()
+    {
+        if (mainCamera == null || lineRenderer == null) return;
+
+        UpdateFrustumLines();
+    }
+
+    private void UpdateFrustumLines()
+    {
+
+        Plane groundPlane = new Plane(Vector3.up, new Vector3(0, minimapLineHeight, 0));
+
+        for (int i = 0; i < 4; i++)
+        {
+            //화면 모서리 Ray 발사
+            Ray ray = mainCamera.ViewportPointToRay(viewportPoints[i]);
+
+            if (groundPlane.Raycast(ray, out float enter))
+            {
+                worldPoints[i] = ray.GetPoint(enter);
+            }
+            else
+            {
+                worldPoints[i] = mainCamera.transform.position + ray.direction * 50f;
+                worldPoints[i].y = minimapLineHeight;
+            }
+        }
+
+        worldPoints[3] = worldPoints[0];
+
+        // Line Renderer에 계산된 4개의 좌표 전달
+        lineRenderer.SetPositions(worldPoints);
+    }
+}
+
